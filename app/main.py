a08bfdda0abd576bcef124f2e80093f60ff40376
Harshit <workwithvrz@gmail.com>
2025-11-11 20:33:24 +0530

    feat(api): link v1 routes to main FastAPI app

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI 
-
+from app.api.v1 import routes_test
 
 app = FastAPI(title="FinGuard API")
 
@@ -7,3 +7,4 @@ app = FastAPI(title="FinGuard API")
 def health_check():
     return {"status":"ok"}
 
+app.include_router(routes_test.router, prefix="/v1")
