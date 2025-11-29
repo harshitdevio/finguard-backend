f848190daabf613eb3291482d05fc132a3e8adf5
Harshit <workwithvrz@gmail.com>
2025-11-29 16:41:59 +0530

    test(integration): add integration_db and integration_app fixtures

--- tests/integration/confest.py
+++ tests/integration/confest.py
@@ -4,3 +4,13 @@ from fastapi import FastAPI
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.main import app as fastapi_app
+
+
+@pytest.fixture()
+async def integration_db(db_session: AsyncSession):
+    yield db_session
+
+
+@pytest.fixture()
+def integration_app() -> FastAPI:
+    return fastapi_app
