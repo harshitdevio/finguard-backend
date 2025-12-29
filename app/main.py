60e2872475ed245edaf9dd98432156ecb66d7d1b
Harshit <workwithvrz@gmail.com>
2025-12-29 18:28:28 +0530

    feat: enable direct execution of app/main.py with uvicorn

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import uvicorn 
 from fastapi import FastAPI 
 from app.api.v1.router import router as v1_router
 
@@ -8,3 +9,6 @@ def health_check():
     return {"status":"ok"}
 
 app.include_router(v1_router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
